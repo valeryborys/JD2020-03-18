d6c24aa1fdbe5c437504096914e9a4320b05b3ca
szamostyanin <sergeyzamostyanin@gmail.com>
2020-03-28 00:59:42 +0300

    TaskA completed

--- src/by/it/szamostyanin/jd01_05/TaskA.java
+++ src/by/it/szamostyanin/jd01_05/TaskA.java
@@ -1,33 +1,42 @@
 package by.it.szamostyanin.jd01_05;
 
+import static java.lang.Math.*; //static import
+
 public class TaskA {
     public static void main(String[] args) {
         step1();
-
+        step2();
+        step3();
     }
 
-    public static void main(String[] args) {
-        double a = 7.556.13;
+    private static void step1() {
+        System.out.println("Task A1");
+        double a = 756.13;
         double x = 0.3;
-        double part1=Math.cos(pow(x*x+Math.PI.6.5));
-        double pART1=SQRT(X*POW(A,3));
-        double part1=log(abs((a-1.12*4)/4));
-        double z = part1 - part2 - part3;
-        System.out.printf("A=$f X=%f Z=%f",a,x,z);
+        double part1 = x * x + Math.PI / 6;
+        double part2 = pow(part1, 5);
+        double part3 = cos(part2); //1 element
+        double part4 = x * pow(a, 3);
+        double part5 = sqrt(part4); //2 element
+        double part6 = log((a - 1.12 * x) / 4); //3element
+        double z = part3 - part5 - part6;
+        System.out.printf("A=%f\nX=%f\nZ=%f\n\n", a, x, z);
     }
 
-    public static void step2 (){
+    private static void step2() {
+        System.out.println("Task A2");
         double a = 1.21;
         double b = 0.371;
-        double y= tan(pow(a+b,2)) -pow(a+1.5,1.3/) ...
-        System.out.printf("A=%f B=%f Y=%f",a,b,y);
+        double y = tan(pow((a + b), 2)) - cbrt(a + 1.5) + a * (pow(b, 5)) - b / log(pow(a, 2));
+        System.out.printf("A=%f\nB=%f\nY=%f\n\n", a, b, y);
     }
-    private static void step3(){
+
+    private static void step3() {
+        System.out.println("Task A3");
         double x = 12.1;
-        System.out.prinln("Task3\nA     F");
-        for (double a = -5; a < 12.1 ; a+= 3.75) {
-            double f = exp(a*x)-3.45 *a;
-            System.out.printf("При  а=%5.2f f=$-13e\n",a,f);
+        for (double a = -5; a <= 12.1; a += 3.75) {
+            double f = exp(a * x) - 3.45 * a;
+            System.out.printf("При  а=%5.2f f=%-13f\n", a, f);
         }
     }
 }
