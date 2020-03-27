2cf8a6385a1834b44df6e7fb7837b0cb446e6fb0
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-03-27 19:30:13 +0200

    jd01_05 TastB2 done

--- src/by/it/gutkovsky/jd01_05/TaskB.java
+++ src/by/it/gutkovsky/jd01_05/TaskB.java
@@ -1,13 +1,44 @@
 package by.it.gutkovsky.jd01_05;
 
 
+import static java.lang.Math.*;
 
 public class TaskB {
     public static void main(String[] args) {
-        step1();
+//        step1();
+        step2();
         
     }
 
     private static void step1() {
+        for (double a = 0; a <=2; a+=0.2) {
+            int x;
+//            for (int x < )
+//            double y = (pow(7,a) - cos(x));
+        }
+
     }
+
+    private static void step2() {
+        for (double x = -5.5; x <=2 ; x+=0.5) {
+            if (x/2 > -2 && x/2<=-1 ){
+                double b = sin(x*x);
+                double a = log10(Math.abs((b+2.74)));
+                System.out.printf("%-7s%3.2f %4s%-13e\n", "при x/2=",x/2, "a = ", a);
+            } else if (x/2 > -1 && x/2 < 0.2) {
+                double b = cos(x*x);
+                double a = log10(Math.abs((b+2.74)));
+                System.out.printf("%-7s%3.2f %4s%-13e\n", "при x/2=",x/2, "a = ", a);
+            } else if (x/2 == 0.2) {
+                double b = cos(x*x) / sin(x*x);
+                double a = log10(Math.abs((b+2.74)));
+                System.out.printf("%-7s%3.2f %4s%-13e\n", "при x/2=",x/2, "a = ", a);
+            } else {
+                System.out.printf("%-6s%3.2f %24s\n","при х/2=",x/2, "вычисления не определены");
+            }
+
+        }
+
+    }
+
 }
