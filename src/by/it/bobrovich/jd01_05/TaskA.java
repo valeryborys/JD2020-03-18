22fc3ce64fc193d6c21fe8da1129d06d8eec1396
chilly_cherry <masha_pleshlo@list.ru>
2020-03-27 12:40:19 +0300

    start jd01_05

--- src/by/it/bobrovich/jd01_05/TaskA.java
+++ src/by/it/bobrovich/jd01_05/TaskA.java
@@ -11,5 +11,13 @@ public class TaskA {
         double part3 = log(abs((a - 1.12 * x) / 4));
         double z = part1 - part2 - part3;
         System.out.println(z);
+        step2();
+    }
+
+    private static void step2() {
+        double a = 1.21;
+        double b = 0.371;
+        double y = tan(pow((a + b), 2)) - pow((a + 1.5), 1 / 3.) + a * pow(b, 5) - b / log(a * a);
+        System.out.println(y);
     }
 }
