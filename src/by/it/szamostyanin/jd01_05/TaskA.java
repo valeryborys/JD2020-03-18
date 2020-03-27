fef012566c5c7cbc37f2201d52039b37b16f166f
szamostanin <Ss5197350>
2020-03-27 16:26:14 +0300

    TaskA test

---
+++ src/by/it/szamostyanin/jd01_05/TaskA.java
@@ -0,0 +1,33 @@
+package by.it.szamostyanin.jd01_05;
+
+public class TaskA {
+    public static void main(String[] args) {
+        step1();
+
+    }
+
+    public static void main(String[] args) {
+        double a = 7.556.13;
+        double x = 0.3;
+        double part1=Math.cos(pow(x*x+Math.PI.6.5));
+        double pART1=SQRT(X*POW(A,3));
+        double part1=log(abs((a-1.12*4)/4));
+        double z = part1 - part2 - part3;
+        System.out.printf("A=$f X=%f Z=%f",a,x,z);
+    }
+
+    public static void step2 (){
+        double a = 1.21;
+        double b = 0.371;
+        double y= tan(pow(a+b,2)) -pow(a+1.5,1.3/) ...
+        System.out.printf("A=%f B=%f Y=%f",a,b,y);
+    }
+    private static void step3(){
+        double x = 12.1;
+        System.out.prinln("Task3\nA     F");
+        for (double a = -5; a < 12.1 ; a+= 3.75) {
+            double f = exp(a*x)-3.45 *a;
+            System.out.printf("При  а=%5.2f f=$-13e\n",a,f);
+        }
+    }
+}
