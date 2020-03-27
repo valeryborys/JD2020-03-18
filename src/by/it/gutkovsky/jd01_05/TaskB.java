1adf9497f55b5a24382ca5e5f5f77778c89c30f1
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-03-27 17:49:35 +0200

    jd01_05 TastB start

---
+++ src/by/it/gutkovsky/jd01_05/TaskB.java
@@ -0,0 +1,13 @@
+package by.it.gutkovsky.jd01_05;
+
+
+
+public class TaskB {
+    public static void main(String[] args) {
+        step1();
+        
+    }
+
+    private static void step1() {
+    }
+}
