3425bb96a4d6c505805a812f12aaf610a2385299
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-03-28 22:00:50 +0200

    jd01_05 TastC2 done

--- src/by/it/gutkovsky/Main.java
+++ src/by/it/gutkovsky/Main.java
@@ -1,13 +1,13 @@
 package by.it.gutkovsky;
 
-import java.util.Arrays;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello word");
-        System.out.println(Math.pow(2, 3));
+
+        int a = 7%2;
+        System.out.println(a);
 
     }
 }
