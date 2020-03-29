b283d3b17ae47b9ed7c12ae1f1d6b10533098d15
Vitaly Tolstik <vital.tol90@gmail.com>
2020-03-29 19:47:22 +0300

    task C jd01_05 completed

--- src/by/it/tolstik/jd01_05/TaskC.java
+++ src/by/it/tolstik/jd01_05/TaskC.java
@@ -4,11 +4,17 @@ import static java.lang.Math.*;
 
 class TaskC {
     public static void main(String[] args) {
+        double[] a = getArrayA();
+        printArray(a, "A");
+        double[] b = sortArrayForMoreThan(a, 3.5);
+        printArray(b, "B");
+        avgGeometrical(b);
+    }
+
+    static double[] getArrayA() {
         double[] a = new double[40];
-        double[] temp = new double[40];
         int i = 0;
         for (double x = 5.33; x <= 9; x += 0.09) {
-
             if (i >= 40) {
                 break;
             } else {
@@ -16,28 +22,39 @@ class TaskC {
                 i++;
             }
         }
-        for (int j = 0; j < a.length; j++) {
-            System.out.printf("%s[%2d]=%10.5f ", "A", j, a[j]);
-            if ((j + 1) % 5 == 0 || i + 1 == a.length) {
+        return a;
+    }
+
+    static void printArray(double[] arr, String name) {
+        for (int j = 0; j < arr.length; j++) {
+            System.out.printf("%s[%2d]=%10.5f ", name, j, arr[j]);
+            if ((j + 1) % 5 == 0 || j + 1 == arr.length) {
                 System.out.println();
             }
         }
+        System.out.println();
+    }
+
+    static double[] sortArrayForMoreThan(double[] arr, double num) {
+        double[] temp = new double[40];
         int count = 0;
-        for (int k = 0, k1 = 0; k < a.length; k++) {
-            if (a[k] >= 3.5) {
-                temp[k1] = a[k];
+        for (int k = 0, k1 = 0; k < arr.length; k++) {
+            if (arr[k] >= num) {
+                temp[k1] = arr[k];
                 k1++;
                 count++;
             }
         }
-        System.out.println();
         double[] b = new double[count];
-        for (int m = 0; m < b.length; m++) {
-            b[m] = temp[m];
-            System.out.printf("%s[%2d]=%10.5f ", "B", m, b[m]);
-            if ((m + 1) % 5 == 0 || i + 1 == a.length) {
-                System.out.println();
-            }
+        System.arraycopy(temp, 0, b, 0, b.length);
+        return b;
+    }
+
+    static void avgGeometrical(double[] arr) {
+        double sum = 0;
+        for (double elem : arr) {
+            sum += elem;
         }
+        System.out.println(pow(sum, 1. / arr.length));
     }
 }
