38139c58a1ceb606def4ec1dd7b57528dd674b4d
Valery <valeryborys@gmail.com>
2020-03-29 23:19:28 +0300

    jd01_04 TaskC complete

--- src/by/it/borys/jd01_04/TaskC.java
+++ src/by/it/borys/jd01_04/TaskC.java
@@ -1,19 +1,14 @@
 package by.it.borys.jd01_04;
 
-/*import by.it.borys.jd01_03.InOut;
+import by.it.borys.jd01_03.InOut;
 
 import java.util.Scanner;
 public class TaskC {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
-       // buildOneDimArray(line);
-       double[] part1 = new double[]{1, 3, 4, 8, 15};
-        double[] part2 = new double[]{40};
-        double[] array = merge(part1, part2);
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i] + " ");
-            }
+        buildOneDimArray(line);
+
     }
 
     static void buildOneDimArray(String line) {
@@ -21,41 +16,61 @@ public class TaskC {
         InOut.printArray(array, "V", 5);
         double first = array[0];
         double last = array[array.length - 1];
-        //mergeSort(array);
-        System.out.println();
+        mergeSort(array);
+        //Helper.sort(array);
+
+       System.out.println();
         InOut.printArray(array, "V", 4);
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == first) {
-                System.out.println("Index of first element=" + i);
-                break;
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == last) {
-                System.out.println("Index of last element=" + i);
-                break;
-            }
-        }
+        System.out.println("first element="+binarySearch(array,first));
+        System.out.println("last element="+binarySearch(array,last));
+
 
     }
 
-    static void mergeSort(double[] array) {
-        int left = 0;
-        int right = array.length - 1;
-        int m = (left + right) / 2;
+   private static double[] mergeSort(double[] array) {
+
+       if (array.length<2)return array;
+       int left = 0;
+       int right = array.length - 1;
+       int m = (left + right) / 2;
+       double[] array1 = new double[m+1];
+       for (int i = 0; i <array1.length ; i++) {
+           array1[i] = array[i];
+       }
+       double[] array2 = new double[right-m];
+       for (int i = 0; i <array2.length ; i++) {
+        array2[i] = array[m+1+i];
+       }
         if (left < right) {
-            merge(mergeSort(array, left, m), mergeSort(array, m + 1, right));
+            array1 = mergeSort(array1);
+            array2 = mergeSort(array2);
+
         }
-       else return;
-    }
-   private static void[] mergeSort(double[] array, int left, int right) {
-        int m = (left + right) / 2;
-        if (left < right) {
-            merge(mergeSort(array, left, m), mergeSort(array, m + 1, right));
-        }
-        else return;
+       double[] arr = merge(array1,array2);
+       for (int i = 0; i < array.length; i++) {
+        array[i] = arr[i];
+       }
+       return array;
 
     }
+   /*private static double[] mergeSort(double[] array, int left, int right) {
+       if (array.length<2)return array;
+          int m = (left + right) / 2;
+       double[] array1 = new double[m];
+       for (int i = 0; i <array1.length ; i++) {
+           array1[i] = array[i];
+       }
+       double[] array2 = new double[array.length-m];
+       for (int i = 0; i <array2.length ; i++) {
+           array2[i] = array[m+i];
+       }
+       if (left < right) {
+           mergeSort(array1, left, m);
+           mergeSort(array2, m + 1, right);
+           return merge(array1,array2);
+       }
+       else return array;
+    }*/
     private static double[] merge(double[] part1, double[] part2) {
         double[] res = new double[part1.length + part2.length];
         int ileft = 0;
@@ -79,7 +94,20 @@ public class TaskC {
         }
         return res;
     }
+
+  static int binarySearch(double[] array, double value){
+      int left = 0;
+      int right = array.length;
+      while (left <= right){
+          int m = (left+right)/2;
+          if (array[m] == value){
+              return m;
+
+          }
+          else if (array[m]>value){
+              right = m-1;
+          }
+          else left = m+1;
+      } return 1000;
+  }
 }
-
-
-*/
