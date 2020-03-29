f219bdccebf292a8fef152c6a29c51e4bba8b7c2
Sergey Lemesh <serrgey.l@gmail.com>
2020-03-29 12:47:03 +0300

    Commit jd01_03

---
+++ src/by/it/lemesh/jd01_03/InOut.java
@@ -0,0 +1,31 @@
+package by.it.lemesh.jd01_03;
+
+import java.util.Arrays;
+
+public class InOut {
+    static double[] getArray(String line) {
+        String[] strArr = line.trim().split(" ");
+        double[] doubleArr = new double[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            doubleArr[i] = Double.parseDouble(strArr[i]);
+        }
+        return doubleArr;
+    }
+
+    static void printArray(double[] arr) {
+        for (double element : arr) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    static void printArray(double[] array, String name, int col) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%s[% -3d] =%7.3f ", name, i, array[i]);
+            if ((i+1)%col == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+}
