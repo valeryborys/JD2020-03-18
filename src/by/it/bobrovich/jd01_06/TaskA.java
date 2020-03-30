03bfa9ba6dfc591fcbffda51a24e7837be1e6839
chilly_cherry <masha_pleshlo@list.ru>
2020-03-30 12:15:48 +0300

    start jd01_06

--- src/by/it/bobrovich/jd01_06/TaskA.java
+++ src/by/it/bobrovich/jd01_06/TaskA.java
@@ -1,4 +1,15 @@
 package by.it.bobrovich.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskA {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{7,}");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
 }
