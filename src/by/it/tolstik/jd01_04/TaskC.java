25b64439490759caf212c316bdd727916ec7bae7
Vitaly Tolstik <vital.tol90@gmail.com>
2020-03-30 11:45:26 +0300

    task C jd01_05 recompleted

--- src/by/it/tolstik/jd01_04/TaskC.java
+++ src/by/it/tolstik/jd01_04/TaskC.java
@@ -62,11 +62,59 @@ class TaskC {
 
     }
 
-    static void mergeSort(double[] array) {
-        Arrays.sort(array);
+
+    private static void mergeSort(double[] array) {
+        double[] example = mergeSort(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = example[i];
+        }
     }
 
-    static int binarySearch(double[] array, double value) {
-        return Arrays.binarySearch(array, value);
+    private static double[] mergeSort(double[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            return merge(mergeSort(array, left, mid), mergeSort(array, mid + 1, right));
+        } else {
+            double[] array1 = {array[left]};
+            return array1;
+        }
+
     }
-}
+
+    private static double[] merge(double part1[], double part2[]) {
+        double array[] = new double[part1.length + part2.length];
+        for (int i = 0, j = 0, k = 0; k < array.length; k++) {
+            if (i == part1.length) {
+                array[k] = part2[j];
+                j++;
+            } else if (j == part2.length) {
+                array[k] = part1[i];
+                i++;
+            } else if (part1[i] < part2[j]) {
+                array[k] = part1[i];
+                i++;
+            } else {
+                array[k] = part2[j];
+                j++;
+            }
+        }
+
+        return array;
+    }
+    private static int binarySearch(double[] array, double value) {
+        int left = 0;
+        int right = array.length - 1;
+        for (int i = 0; i < array.length; i++) {
+            int mid = (left + right) / 2;
+            if (array[mid] == value) {
+                return mid;
+            } else if (array[mid] < value) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return 0;
+    }
+    }
+
