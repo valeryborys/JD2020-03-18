4c07e9130c1a975357cac292efc9129f78032cc9
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-03-30 12:29:57 +0300

    - Задание А (местами сделанное)

---
+++ src/by/it/okatov/jd01_06/TaskB.java
@@ -0,0 +1,18 @@
+package by.it.okatov.jd01_06;
+
+public class TaskB {
+    public static void main(String[] args) {
+        step1();
+        step2();
+    }
+
+    private static void step1() {
+
+    }
+
+    private static void step2() {
+
+    }
+
+
+}
---
+++ src/by/it/okatov/jd01_06/TaskC.java
@@ -0,0 +1,18 @@
+package by.it.okatov.jd01_06;
+
+public class TaskC {
+    public static void main(String[] args) {
+        step1();
+        step2();
+    }
+
+    private static void step1() {
+
+    }
+
+    private static void step2() {
+
+    }
+
+
+}
