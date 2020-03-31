969741a57e9ec6eb1be6bbc9793a767f24d78868
PsyXodelic <beavkostya@gmail.com>
2020-03-31 20:10:32 +0300

    jd01_06

--- src/by/it/bobrovich/jd01_06/TaskB2.java
+++ src/by/it/bobrovich/jd01_06/TaskB2.java
@@ -7,11 +7,19 @@ import java.util.regex.Pattern;
 public class TaskB2 {
     public static void main(String[] args) {
         StringBuilder text = new StringBuilder(Poem.text);
-        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ, -:\n]+");
+        Pattern pattern = Pattern.compile("((\\d+\\.\\s*)*(|[^?!.\\(]|\\([^\\)]*\\))*[.?!])");
         Matcher matcher = pattern.matcher(text);
-        while(matcher.find()){
-            String text1 = matcher.group();
+        Pattern pattern1 = Pattern.compile("[^а-яА-ЯёЁ]");
+        while (matcher.find()) {
+            StringBuilder text1 = new StringBuilder(matcher.group());
+            Matcher matcher1 = pattern1.matcher(text1);
+            while (matcher1.find()) {
+                int position = matcher1.start();
+                text1.setCharAt(position, ' ');
+            }
+            //String s = text1.toString();
             System.out.println(text1);
+            System.out.println();
         }
     }
 }
