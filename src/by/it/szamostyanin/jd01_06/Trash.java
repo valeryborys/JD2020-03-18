ec1d2cdb7b3d5000a8ea30bfd8a79f43a6b11103
szamostyanin <sergeyzamostyanin@gmail.com>
2020-03-31 18:32:47 +0300

    jd01_06 TaskA1 completed

---
+++ src/by/it/szamostyanin/jd01_06/Trash.java
@@ -0,0 +1,10 @@
+package by.it.szamostyanin.jd01_06;
+
+public class Trash {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("qwerty");
+        str.setCharAt(3, '#');
+        System.out.println(str);
+        //r->X
+    }
+}
