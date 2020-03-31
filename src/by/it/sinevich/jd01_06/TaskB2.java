b584e58683ae22731d56d45c3a06fe59465032d0
sinevich <sinevichaleksandr@gmail.com>
2020-04-01 00:02:27 +0300

    start jd01-05

---
+++ src/by/it/sinevich/jd01_06/TaskB2.java
@@ -0,0 +1,11 @@
+package by.it.sinevich.jd01_06;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+
+}
