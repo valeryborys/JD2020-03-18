76e8a397b18021fef7f79b49b941ec9300e10444
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-04-02 23:26:37 +0300

    add jd01_07 task B+C

--- src/by/it/verbitsky/jd01_07/Runner.java
+++ src/by/it/verbitsky/jd01_07/Runner.java
@@ -2,13 +2,47 @@ package by.it.verbitsky.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
+        System.out.println("Testing Task A");
         Var var = new Scalar(3.1415);
         Var varSc = new Scalar("3.1415");
         Scalar sc = new Scalar(3.1415);
         Scalar scalar = new Scalar(sc);
+
         System.out.println(var.toString());
         System.out.println(varSc.toString());
-        System.out.println(sc.toString());
         System.out.println(scalar.toString());
+
+        System.out.println("\nTesting Task B");
+        double[] doubleArray = {1.0, 2.0, 4.0};
+        double[] doubleArray2 = {3.0, 4.0, 5.0};
+        String stringArray = "{1.0, 2.0, 4.0}";
+
+        Var varB = new Vector(doubleArray);
+        Var varBstr = new Vector(stringArray);
+        Vector vectorX = new Vector(doubleArray2);
+        Var varFromVector = new Vector(vectorX);
+
+        System.out.println("Vector from array");
+        System.out.println(varB.toString());
+        System.out.println("Vector from String");
+        System.out.println(varBstr.toString());
+        System.out.println("Vector from Vector");
+        System.out.println(varFromVector.toString());
+
+        System.out.println("\nTesting Task C");
+        double[][] matrixDouble = {{1.0, 2.0}, {3.0, 4.0}};
+        double[][] matrixDouble2 = {{3.0, 4.0}, {5.0, 6.0}};
+
+        Var varC = new Matrix(matrixDouble);
+        Matrix matrixZ = new Matrix(matrixDouble2);
+        Var varFromMatrix = new Matrix(matrixZ);
+        Var varCstr = new Matrix("{{1.2, 2.3, 3.2}, {4,5,6}}");
+
+        System.out.println("Matrix from array");
+        System.out.println(varC.toString());
+        System.out.println("Matrix from Matrix");
+        System.out.println(varFromMatrix.toString());
+        System.out.println("Matrix from String");
+        System.out.println(varCstr.toString());
     }
 }
--- src/by/it/verbitsky/jd01_07/Vector.java
+++ src/by/it/verbitsky/jd01_07/Vector.java
@@ -1,7 +1,49 @@
 package by.it.verbitsky.jd01_07;
 
+import java.util.Arrays;
+
 class Vector extends Var {
     //arays.coрy для конструктора с вектором
     //для стринга - парсить через регулярку
     //to string через билдер
+
+    private double[] vector;
+
+    Vector(double[] array) {
+        this.vector = Arrays.copyOf(array, array.length);
+    }
+
+    Vector(Vector vector) {
+        this.vector = Arrays.copyOf(vector.getVector(), vector.getVector().length);
+    }
+
+    Vector(String strVector) {
+        this.vector = getVectorFromString(strVector);
+    }
+
+    private double[] getVectorFromString(String strVector) {
+        strVector = strVector.replaceAll("[{}\\s]", "");
+        String[] strings = strVector.split(",");
+        double[] array = new double[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            array[i] = Double.parseDouble(strings[i]);
+        }
+        return array;
+    }
+
+    private double[] getVector() {
+        return vector;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
+        for (double v : getVector()) {
+            stringBuilder.append(v).append(", ");
+        }
+        stringBuilder.setCharAt(stringBuilder.length() - 2, '}');
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1).trimToSize();
+        return stringBuilder.toString();
+    }
 }
