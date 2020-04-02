6e3aac5f071baa9ffd76c874446efff40528f24e
Valery <valeryborys@gmail.com>
2020-04-02 12:19:18 +0300

    jd01_07 TaskA TaskB complited

--- src/by/it/borys/jd01_06/TaskB2.java
+++ src/by/it/borys/jd01_06/TaskB2.java
@@ -1,19 +1,28 @@
 package by.it.borys.jd01_06;
 
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskB2 {
-   /* public static void main(String[] args) {
+   public static void main(String[] args) {
         StringBuilder sb = new StringBuilder(Poem.text);
+        Pattern p0 = Pattern.compile("\\.\\.\\.");
+        Matcher m0 = p0.matcher(sb);
+        m0.find();
+           int threepoints = m0.start();
+        int threepointsend = m0.end();
+        sb.replace(threepoints,threepointsend,"&");
+
         Pattern p1 = Pattern.compile("\\s*+[^а-яА-яёЁ\\.]+\\s*");
-        Matcher m1 = p1.matcher(Poem.text);
-        while (m1.find()){
+        Matcher m1 = p1.matcher(sb);
+        int pos=0;
+        while (m1.find(pos)){
             int start = m1.start();
             int end = m1.end();
-            sb.replace(start,end,"1");
+            sb.replace(start,end," ");
+            pos=start+1;
         }
 
         System.out.println(sb);
-    }*/
+    }
 }
---
+++ src/by/it/borys/jd01_07/Runner.java
@@ -0,0 +1,10 @@
+package by.it.borys.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+Var v1 = new Scalar("3.1415");
+Var v2 = new Vector("58.5} 2 5, g4 к б ,8");
+        System.out.println(v1);
+        System.out.println(v2);
+    }
+}
