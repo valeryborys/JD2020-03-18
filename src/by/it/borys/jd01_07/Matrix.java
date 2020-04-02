0171fa5afab8c125c4f717bd23d17132127d0678
Valery <valeryborys@gmail.com>
2020-04-02 16:21:08 +0300

    jd01_07 complete

--- src/by/it/borys/jd01_07/Matrix.java
+++ src/by/it/borys/jd01_07/Matrix.java
@@ -1,6 +1,9 @@
 package by.it.borys.jd01_07;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Matrix extends Var {
     private double[][] value;
 
@@ -10,6 +13,37 @@ class Matrix extends Var {
 
     Matrix(Matrix matrix){
         this.value = matrix.value;
+    }
+
+    Matrix(String strMatrix){
+        String[] rows = strMatrix.split("}");
+        for (int i = 0; i < rows.length; i++) {
+            StringBuilder sb = new StringBuilder(rows[i]);
+            Pattern p1 = Pattern.compile("\\s*+[^0-9.]+\\s*");
+            Matcher m1 = p1.matcher(sb);
+            int pos=0;
+            while (m1.find(pos)){
+                int start = m1.start();
+                int end = m1.end();
+                sb.replace(start,end," ");
+                pos=start+1;
+            }
+            rows[i]=sb.toString().trim();
+        }
+        int[] countcols = new int[rows.length];
+        String[][] cols = new String[rows.length][];
+        double[][] matrix = new double[rows.length][];
+        for (int i = 0; i < rows.length; i++) {
+            cols[i] = rows[i].split(" ");
+            countcols[i] = cols[i].length;
+            matrix[i] = new double[countcols[i]];
+            for (int j = 0; j < matrix[i].length; j++) {
+             matrix[i][j] = Double.parseDouble(cols[i][j]);
+            }
+                this.value = matrix;
+    }
+
+
     }
 
     @Override
--- src/by/it/borys/jd01_07/Runner.java
+++ src/by/it/borys/jd01_07/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
     public static void main(String[] args) {
 Var v1 = new Scalar("3.1415");
 Var v2 = new Vector("58.5} 2 5, g4 к б ,8");
-Var v3 = new Matrix(new double[][] {{1,2}, {2,3}});
+Var v3 = new Matrix("{{1,4,5}, {1}, {5,8.04,2,8.5,0.2}}");
         System.out.println(v1);
         System.out.println(v2);
         System.out.println(v3);
