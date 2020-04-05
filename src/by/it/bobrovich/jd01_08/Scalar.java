e36f8e0a8452c40fb8640b0c82470ef88088a5c7
PsyXodelic <beavkostya@gmail.com>
2020-04-05 16:58:18 +0300

    jd01_08

--- src/by/it/bobrovich/jd01_08/Scalar.java
+++ src/by/it/bobrovich/jd01_08/Scalar.java
@@ -1,6 +1,6 @@
 package by.it.bobrovich.jd01_08;
 
-public class Scalar extends Var{
+public class Scalar extends Var {
     private double value;
 
     public Scalar(double value) {
@@ -17,34 +17,43 @@ public class Scalar extends Var{
 
     @Override
     public Var add(Var other) {
-        if (other instanceof Scalar){
+        if (other instanceof Scalar) {
             double sum = this.value + ((Scalar) other).value;
             return new Scalar(sum);
-        }else return other.add(this);
+        } else return other.add(this);
     }
 
     @Override
     public Var sub(Var other) {
-        if (other instanceof Scalar){
+        if (other instanceof Scalar) {
             double result = this.value - ((Scalar) other).value;
             return new Scalar(result);
-        }else return this.sub(other);
+        } else return new Scalar(-1).mul(other).add(this);
     }
 
     @Override
     public Var mul(Var other) {
-        if (other instanceof Scalar){
+        if (other instanceof Scalar) {
             double result = this.value * ((Scalar) other).value;
             return new Scalar(result);
-        }else return other.mul(this);
+        } else return other.mul(this);
     }
 
     @Override
     public Var div(Var other) {
-        if(other instanceof Scalar){
-            double result = this.value / ((Scalar) other).value;
-            return new Scalar(result);
-        }else return this.div(other);
+        if (other instanceof Scalar) {
+            if (((Scalar) other).value != 0) {
+                double result = this.value / ((Scalar) other).value;
+                return new Scalar(result);
+            } else {
+                System.out.println("Div by Zero");
+                return null;
+            }
+        } else return super.div(other);
+    }
+
+    public double getValue() {
+        return value;
     }
 
     @Override
--- src/by/it/bobrovich/jd01_08/Vector.java
+++ src/by/it/bobrovich/jd01_08/Vector.java
@@ -19,7 +19,7 @@ public class Vector extends Var {
         StringBuilder sb = new StringBuilder(strVector);
         Pattern pattern = Pattern.compile("[{}]");
         Matcher matcher = pattern.matcher(sb);
-        while(matcher.find()){
+        while (matcher.find()) {
             int position = matcher.start();
             sb.setCharAt(position, ' ');
         }
@@ -30,6 +30,79 @@ public class Vector extends Var {
         }
         value = Arrays.copyOf(value1, value1.length);
 
+    }
+
+    @Override
+    public Var add(Var other) {
+        double[] newVector = new double[this.value.length];
+        if (other instanceof Vector) {
+            if (this.value.length == ((Vector) other).value.length) {
+                for (int i = 0; i < this.value.length; i++) {
+                    for (double aValue : ((Vector) other).value) {
+                        newVector[i] = this.value[i] + ((Vector) other).value[i];
+                    }
+                }
+            }
+            return new Vector(newVector);
+        } else if (other instanceof Scalar) {
+            for (int i = 0; i < this.value.length; i++) {
+                newVector[i] = this.value[i] + ((Scalar) other).getValue();
+            }
+            return new Vector(newVector);
+        } else return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        double[] newVector = new double[this.value.length];
+        if (other instanceof Vector) {
+            if (this.value.length == ((Vector) other).value.length) {
+                for (int i = 0; i < this.value.length; i++) {
+                    newVector[i] = this.value[i] - ((Vector) other).value[i];
+                }
+            }
+            return new Vector(newVector);
+        } else if (other instanceof Scalar) {
+            for (int i = 0; i < this.value.length; i++) {
+                newVector[i] = this.value[i] - ((Scalar) other).getValue();
+            }
+            return new Vector(newVector);
+        } else return super.add(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        double[] newVector = new double[this.value.length];
+        double result = 0.0;
+        if (other instanceof Vector) {
+            if (this.value.length == ((Vector) other).value.length) {
+                for (int i = 0; i < this.value.length; i++) {
+                    result += this.value[i] * ((Vector) other).value[i];
+                }
+            }
+            return new Scalar(result);
+        } else if (other instanceof Scalar) {
+            for (int i = 0; i < this.value.length; i++) {
+                newVector[i] = this.value[i] * ((Scalar) other).getValue();
+            }
+            return new Vector(newVector);
+        } else return super.add(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        double[] newVector = new double[this.value.length];
+        if (other instanceof Scalar) {
+            if (((Scalar) other).getValue() != 0) {
+                for (int i = 0; i < this.value.length; i++) {
+                    newVector[i] = this.value[i] / ((Scalar) other).getValue();
+                }
+                return new Vector(newVector);
+            } else {
+                System.out.println("Div by zero");
+                return null;
+            }
+        } else return super.div(other);
     }
 
     @Override
