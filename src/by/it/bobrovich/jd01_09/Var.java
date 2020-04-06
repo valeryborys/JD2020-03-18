51896bddf46a73a92650abb8d6335b0ffb5e8bec
PsyXodelic <beavkostya@gmail.com>
2020-04-06 16:51:08 +0300

    jd01_09

---
+++ src/by/it/bobrovich/jd01_09/Var.java
@@ -0,0 +1,27 @@
+package by.it.bobrovich.jd01_09;
+
+public abstract class Var implements Operation {
+    @Override
+    public Var add(Var other) {
+        System.out.println("Impossible operation");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Impossible operation");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Impossible operation");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Impossible operation");
+        return null;
+    }
+}
