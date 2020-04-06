4bfbbcc19c644c5d089dba8c7f4c8fa6d98ef5ff
Valery <valeryborys@gmail.com>
2020-04-06 20:32:06 +0300

    jd01_09 comlete

---
+++ src/by/it/borys/jd01_09/Parser.java
@@ -0,0 +1,32 @@
+package by.it.borys.jd01_09;
+
+import java.util.regex.Matcher;
+
+public class Parser {
+
+    Var calc(String expression) {
+        String[] operand = expression.split(Pattern.OPERATION);
+        Var one = Var.createVar(operand[0]);
+        Var two = Var.createVar(operand[1]);
+        if (one == null || two == null) return null;//туду
+        java.util.regex.Pattern p1 = java.util.regex.Pattern.compile(Pattern.OPERATION);
+        Matcher m1 = p1.matcher(expression);
+        if (m1.find()) {
+            String operation = m1.group();
+            switch (operation) {
+                case "+":
+                    return one.add(two);
+
+                case "-":
+                    return one.sub(two);
+                case "*":
+                    return one.mul(two);
+                case "/":
+                    return one.div(two);
+                default:
+                    return null;//туду
+            }
+        }
+        return null;
+    }
+}
