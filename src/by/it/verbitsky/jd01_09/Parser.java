83226f0145f6c95e94f5492eccf823bf0bba834b
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-04-06 21:28:32 +0300

    start work jd01-08

---
+++ src/by/it/verbitsky/jd01_09/Parser.java
@@ -0,0 +1,29 @@
+package by.it.verbitsky.jd01_09;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Parser {
+    Var calc (String expression) {
+        String[] parts = expression.split(Patterns.OPERATION, 2);
+        Var left = Var.createVar(parts[0]);
+        if (parts.length ==1) {
+            return left;
+        }
+        Var right = Var.createVar(parts[1]);
+
+        Pattern pattern = Pattern.compile(Patterns.OPERATION);
+        Matcher matcher = pattern.matcher(expression);
+
+        if (matcher.find()) {
+            String operation = matcher.group();
+            switch (operation) {
+                case "+" : return left.add(right);
+                case "-" : return left.sub(right);
+                case "*" : return left.mul(right);
+                case "/" : return left.div(right);
+            }
+        }
+        return null;
+    }
+}
---
+++ src/by/it/verbitsky/jd01_09/Var.java
@@ -0,0 +1,44 @@
+package by.it.verbitsky.jd01_09;
+
+abstract class Var implements Operation {
+    public static Var createVar(String strVar) {
+        if (strVar.matches(Patterns.SCALAR)) {
+            return new Scalar(strVar);
+        }
+
+        //дописать про матрицу и вектор
+
+        return  null;
+    }
+
+    @Override
+    public String toString() {
+        return " Abstract Var";
+    }
+
+    @Override
+    public Var add(Var other) {
+        System.out.println("Вызван метод ADD Абстрактного класса VAR");
+        System.out.printf("\nOperation 'add' impossible  with arguments: %s and %s ", this.toString(), other.toString());
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.printf("\nOperation 'sub' impossible  with arguments: %s and %s ", this.toString(), other.toString());
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.printf("\nOperation 'mul' impossible  with arguments: %s and %s ", this.toString(), other.toString());
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.printf("\nOperation 'div' impossible  with arguments: %s and %s ", this.toString(), other.toString());
+        return null;
+    }
+
+}
