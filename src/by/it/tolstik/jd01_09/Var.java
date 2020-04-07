ee4746ab344cb0bed970b8daadb93388d2b96825
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-07 15:27:23 +0300

    end jd01_09

--- src/by/it/tolstik/jd01_09/Var.java
+++ src/by/it/tolstik/jd01_09/Var.java
@@ -1,6 +1,17 @@
 package by.it.tolstik.jd01_09;
 
-abstract class Var implements Operation {
+abstract class Var implements Operation{
+
+    static Var createVar(String operand){
+        operand = operand.trim().replaceAll("\\s","");
+        if (operand.matches(Patterns.SCALAR))
+            return new Scalar(operand);
+        if (operand.matches(Patterns.VECTOR))
+            return new Vector(operand);
+        if (operand.matches(Patterns.MATRIX))
+            return new Matrix(operand);
+        return null;
+    }
     @Override
     public Var add(Var other) {
         System.out.println("Операция сложения " + this + " + " + other + " не возможна.");
@@ -9,19 +20,19 @@ abstract class Var implements Operation {
 
     @Override
     public Var sub(Var other) {
-        System.out.println("Операция вычитания " + this + " + " + other + " не возможна.");
+        System.out.println("Операция вычитания " + this + " - " + other + " не возможна.");
         return null;
     }
 
     @Override
     public Var mul(Var other) {
-        System.out.println("Операция умножения " + this + " + " + other + " не возможна.");
+        System.out.println("Операция умножения " + this + " * " + other + " не возможна.");
         return null;
     }
 
     @Override
     public Var div(Var other) {
-        System.out.println("Операция деления " + this + " + " + other + " не возможна.");
+        System.out.println("Операция деления " + this + " / " + other + " не возможна.");
         return null;
     }
 
