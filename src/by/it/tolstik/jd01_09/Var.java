fb6b0e9252a400a918f2576cae65edf8feffe8e9
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-07 12:30:22 +0300

    start jd01_09

---
+++ src/by/it/tolstik/jd01_09/Var.java
@@ -0,0 +1,32 @@
+package by.it.tolstik.jd01_09;
+
+abstract class Var implements Operation {
+    @Override
+    public Var add(Var other) {
+        System.out.println("Операция сложения " + this + " + " + other + " не возможна.");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Операция вычитания " + this + " + " + other + " не возможна.");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Операция умножения " + this + " + " + other + " не возможна.");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Операция деления " + this + " + " + other + " не возможна.");
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "You must override toString in child class!";
+    }
+}
