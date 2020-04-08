fe54957d1cde57ba01cdcba079e9ef7cf34e431b
PsyXodelic <beavkostya@gmail.com>
2020-04-08 19:38:25 +0300

    jd01_09

--- src/by/it/bobrovich/jd01_09/Var.java
+++ src/by/it/bobrovich/jd01_09/Var.java
@@ -1,6 +1,17 @@
 package by.it.bobrovich.jd01_09;
 
 public abstract class Var implements Operation {
+
+    static Var createVar(String value){
+        if(value.matches(Patterns.SCALAR))
+            return new Scalar(value);
+        if(value.matches(Patterns.VECTOR))
+            return new Vector(value);
+        if(value.matches(Patterns.MATRIX))
+            return new Matrix(value);
+        return null;
+    }
+
     @Override
     public Var add(Var other) {
         System.out.println("Impossible operation");
