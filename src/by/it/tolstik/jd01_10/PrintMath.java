102813a414c190d7aa736266445b27555fd6a984
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-09 15:09:13 +0300

    start jd01_10

---
+++ src/by/it/tolstik/jd01_10/PrintMath.java
@@ -0,0 +1,4 @@
+package by.it.tolstik.jd01_10;
+
+public class PrintMath {
+}
---
+++ src/by/it/tolstik/jd01_10/Runner.java
@@ -0,0 +1,9 @@
+package by.it.tolstik.jd01_10;
+
+class Runner {
+
+    public static void main(String[] args) {
+
+
+    }
+}
