a17b053a61082dc603fca92de8df22f487b63707
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-09 17:06:42 +0300

    full A,B1 completed jd01_10

--- src/by/it/tolstik/jd01_10/Bean.java
+++ src/by/it/tolstik/jd01_10/Bean.java
@@ -4,17 +4,20 @@ public class Bean {
     static double sum(int a, int b) {
         return a + b;
     }
-    @Param(a=2,b=3)
+
+    @Param(a = 2, b = 3)
     static double max(int a, int b) {
         return Math.max(a, b);
     }
-    @Param(a=1,b=1)
+
+    @Param(a = 1, b = 1)
     double min(int a, int b) {
         return Math.min(a, b);
     }
-    @Param(a=4,b=5)
+
+    @Param(a = 4, b = 5)
     double avg(int a, int b) {
-        return (a+b)/2.0;
+        return (a + b) / 2.0;
     }
 
 }
