cc7eb940a30e535b220f40b52446f662e8339baa
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-10 18:17:46 +0300

    Gutkovsky JD01_11 TaskA1 start

---
+++ src/by/it/gutkovsky/jd01_11/Runner.java
@@ -0,0 +1,21 @@
+package by.it.gutkovsky.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Runner {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+
+        System.out.println(list);
+
+
+
+    }
+}
