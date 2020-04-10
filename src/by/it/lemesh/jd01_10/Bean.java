f2e7e0dd61f98a16d7551692bf12853d1af2e073
Sergey Lemesh <serrgey.l@gmail.com>
2020-04-10 19:00:25 +0300

    jd01_10 end

--- src/by/it/lemesh/jd01_10/Bean.java
+++ src/by/it/lemesh/jd01_10/Bean.java
@@ -1,17 +1,17 @@
 package by.it.lemesh.jd01_10;
 
 public class Bean {
-    @Param(a=7, b=8)
+    @Param(a=3, b=4)
     double sum(int a, int b){
         return a+b;
     }
 
-    @Param(a=7, b=8)
+    @Param(a=5, b=11)
     double avg(int a, int b){
         return (a+b)/2.0;
     }
 
-    @Param(a=7, b=8)
+    @Param(a=2, b=8)
     static double min(int a, int b){
         return Math.min(a, b);
     }
