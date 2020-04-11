4cef88f6ad0081e819116ca39345994b540031ed
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-04-11 11:49:17 +0300

    - Сделано TaskA - В процессе TaskB

---
+++ src/by/it/okatov/jd01_11/Runner.java
@@ -0,0 +1,17 @@
+package by.it.okatov.jd01_11;
+
+
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> list = new ListA<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("five");
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+    }
+}
