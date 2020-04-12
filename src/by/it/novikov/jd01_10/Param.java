b4376571bde1b0c5288c40e09695027b63e4a683
novikov1 <mnovikov1310@gmail.com>
2020-04-12 16:30:05 +0300

    JD01-10

---
+++ src/by/it/novikov/jd01_10/Param.java
@@ -0,0 +1,14 @@
+package by.it.novikov.jd01_10;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@interface Param {
+    int a();
+    int b();
+}
