ff0f4afc6b96ce5094e462653d74834d0efa663c
Sergey Lemesh <serrgey.l@gmail.com>
2020-04-12 17:31:18 +0300

    calc end

---
+++ src/by/it/lemesh/calc/Patterns.java
@@ -0,0 +1,10 @@
+package by.it.lemesh.calc;
+
+public class Patterns {
+
+    static final String OPERATION = "[-=+*/]";
+    static final String SCALAR = "-?[0-9]+\\.?[0-9]*";
+    static final String VECTOR = "\\{(("+SCALAR+"),?)+}";
+    static final String MATRIX = "\\{("+VECTOR+",?)+}";
+
+}
