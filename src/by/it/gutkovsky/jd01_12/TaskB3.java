4eacc4d996ab6f7a810cb1ebbe1585f22de65956
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-15 01:33:14 +0300

    Gutkovsky JD01_12 TaskC3 completed, TaskB3 in pursuit

--- src/by/it/gutkovsky/jd01_12/TaskB3.java
+++ src/by/it/gutkovsky/jd01_12/TaskB3.java
@@ -1,9 +1,6 @@
 package by.it.gutkovsky.jd01_12;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Random;
+import java.util.*;
 
 class TaskB3 {
     public static void main(String[] args) {
@@ -11,19 +8,20 @@ class TaskB3 {
         Random random = new Random(123L);
         ArrayList<String> list1 = new ArrayList<>();
         for (int i = 0; i < 4096; i++) {
-            list1.add("name" + random.nextInt(5000));
+//            list1.add("name" + random.nextInt(5000));
+            list1.add("name" + i);
         }
 
         LinkedList<String> list2 = new LinkedList<>();
         for (int i = 0; i < 4096; i++) {
-            list2.add("name" + random.nextInt(5000));
+            list2.add("name" + i);
         }
-        Timer t1=new Timer();
+        Timer t1 = new Timer();
         process(list1);
-        System.out.println("Время работы для  ArrayList=" + t1 );
+        System.out.println("Время работы для  ArrayList=" + t1);
 //        Timer t2=new Timer();
         process(list2);
-        System.out.println("Время работы для LinkedList=" +t1);
+        System.out.println("Время работы для LinkedList=" + t1);
     }
 
     static String process(ArrayList<String> peoples) {
@@ -40,6 +38,7 @@ class TaskB3 {
         return peoplesCopy.get(0);
     }
 
+
     static String process(LinkedList<String> peoples) {
         LinkedList<String> peoplesCopy = new LinkedList<>(peoples);
         int even = 1;
@@ -54,21 +53,37 @@ class TaskB3 {
         return peoplesCopy.get(0);
     }
 
-    private static class Timer {
-        private  long iniTime;
-        private Double Delta;
-        public Timer(){
-            iniTime= System.nanoTime();
+/*   // Метод с Queue работает неверно - дорабоать!
+    static String process(LinkedList<String> peoples) {
+        int even = 1;
+        Queue<String> peoplesCopy = new LinkedList<>();
+        while (peoplesCopy.size() != 1) {
+            if (even % 2.0 == 0) {
+                ((Queue<String>) peoples).poll();
+            } else {
+                peoplesCopy.offer(peoplesCopy.poll());
+            }
+            even++;
         }
-        public String toString(){
-            Delta=(double)(System.nanoTime()-iniTime)/1000;
-            iniTime= System.nanoTime();
-
-            return Delta.toString()+" мкс";
-        }
+        return peoplesCopy.element();
     }
 
+ */
+    private static class Timer {
+        private long iniTime;
+        private Double Delta;
 
+        public Timer() {
+            iniTime = System.nanoTime();
+        }
+
+        public String toString() {
+            Delta = (double) (System.nanoTime() - iniTime) / 1000;
+            iniTime = System.nanoTime();
+
+            return Delta.toString() + " мкс";
+        }
+    }
 
 
 }
