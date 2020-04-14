1be3b224e429e90135ac32f648f0289cec584691
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-14 14:27:17 +0300

    Gutkovsky JD01_12 TaskB3 in progress

--- src/by/it/gutkovsky/jd01_12/TaskB3.java
+++ src/by/it/gutkovsky/jd01_12/TaskB3.java
@@ -10,16 +10,19 @@ class TaskB3 {
         // создание двух списков людей
         Random random = new Random(123L);
         ArrayList<String> list1 = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 4096; i++) {
             list1.add("name" + random.nextInt(5000));
         }
         LinkedList<String> list2 = new LinkedList<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 4096; i++) {
             list2.add("name" + random.nextInt(5000));
         }
-
-        System.out.println(process(list1));
-        System.out.println(process(list2));
+        Timer t1=new Timer();
+        process(list1);
+        System.out.println("Время работы для  ArrayList=" + t1 );
+        Timer t2=new Timer();
+        process(list2);
+        System.out.println("Время работы для LinkedList=" +t2);
     }
 
     static String process(ArrayList<String> peoples) {
@@ -49,4 +52,25 @@ class TaskB3 {
         }
         return peoplesCopy.get(0);
     }
+
+    private static class Timer {
+        private  long iniTime;
+        private Double Delta;
+        public Timer(){
+            iniTime= System.nanoTime();
+        }
+        public String toString(){
+            Delta=(double)(System.nanoTime()-iniTime)/1000;
+            iniTime= System.nanoTime();
+
+            return Delta.toString()+" мкс";
+        }
+    }
+
+
+
+
 }
+
+
+
