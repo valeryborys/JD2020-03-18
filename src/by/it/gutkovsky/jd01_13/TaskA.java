c135766971e0eddb8e8f13787522b5d5a5fb1829
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-15 17:24:25 +0300

    Gutkovsky JD01_13 TaskA2 completed

---
+++ src/by/it/gutkovsky/jd01_13/TaskA.java
@@ -0,0 +1,28 @@
+package by.it.gutkovsky.jd01_13;
+
+import java.util.HashMap;
+
+public class TaskA {
+    public static void main(String[] args) {
+        try {
+            if (Math.random() > 0.5) {
+                new HashMap<String, String>(null);
+            } else Integer.parseInt("привет");
+        } catch (Exception e) {
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement element : stackTrace) {
+                if (TaskA.class.getName().equals(element.getClassName())) {
+                    String name = e.getClass().getName();
+                    String clname = element.getClassName();
+                    int line = element.getLineNumber();
+                    System.out.printf(
+                             " name: %s\n" +
+                             "class: %s\n" +
+                             " line: %d",
+                             name, clname, line);
+                    break;
+                }
+            }
+        }
+    }
+}
