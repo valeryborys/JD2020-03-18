5f54573220cad01fc3362c48b8cf09704ab05a06
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-15 23:05:20 +0300

    Gutkovsky JD01_13 TaskA1 rev2 completed

--- src/by/it/gutkovsky/jd01_13/TaskA.java
+++ src/by/it/gutkovsky/jd01_13/TaskA.java
@@ -2,14 +2,16 @@ package by.it.gutkovsky.jd01_13;
 
 import java.util.HashMap;
 
-public class TaskA {
+class TaskA {
     public static void main(String[] args) {
+
+        /*
         try {
             if (Math.random() > 0.5) {
                 new HashMap<String, String>(null);
 
             } else Integer.parseInt("привет");
-        } catch (Exception e) {
+        } catch (NumberFormatException | NullPointerException e) {
             StackTraceElement[] stackTrace = e.getStackTrace();
             for (StackTraceElement element : stackTrace) {
                 if (TaskA.class.getName().equals(element.getClassName())) {
@@ -21,6 +23,28 @@ public class TaskA {
                              "class: %s\n" +
                              " line: %d",
                              name, clname, line);
+                    break;
+                }
+            }
+        }
+
+         */
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        } catch (NumberFormatException | NullPointerException e) {
+//            Class<? extends RuntimeException> aClass = e.getClass();
+//            System.out.println("name:" + aClass.getName());
+            StackTraceElement[] trace = e.getStackTrace();
+            for (StackTraceElement element : trace) {
+                if (element.getClassName().equals(TaskA.class.getName())) {
+                    System.out.printf(
+                            " name: %s\n" +
+                                    "class: %s\n" +
+                                    " line: %d",
+                            e.getClass().getName(), element.getClassName(), element.getLineNumber());
                     break;
                 }
             }
