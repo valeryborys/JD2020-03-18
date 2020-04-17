2caafa568fdb70457d6598ff522ce24374f1c25d
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-04-17 13:56:33 +0300

    - Пытаюсь разобраться с форматированием

--- src/by/it/okatov/jd01_14/TaskA.java
+++ src/by/it/okatov/jd01_14/TaskA.java
@@ -3,9 +3,11 @@ package by.it.okatov.jd01_14;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class TaskA {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
         String simpleName = "dataTaskA.bin";
         String fileName = getFileName(TaskA.class, simpleName);
         List<Integer> arrList = new ArrayList<>();
@@ -16,48 +18,12 @@ public class TaskA {
 
         //Читает содержимое файла и записывает его в ArrayList
         // после чего выводит в консоль и считает среднее из всех чисел
-        sum = readToArrayList(fileName, arrList, sum);
+        readToArrayList(fileName, arrList, sum);
 
         //Создает новый файл и записывает в него все числа из старого через
         // пробел, добавляя строку с подсчетом среднего арифметического этих чисел
         simpleName = "resultTaskA.txt";
         writeInputToFile(getFileName(TaskA.class, simpleName), arrList, sum);
-    }
-
-    private static void writeInputToFile(String fileName, List<Integer> arrList, double sum) {
-        try (PrintWriter print = new PrintWriter(fileName)) {
-            for (Integer i : arrList) {
-                print.printf("%d ", i);
-                sum += i;
-            }
-            print.printf("%navg=%f", sum / arrList.size());
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static double readToArrayList(String fileName, List<Integer> arrList, double sum) {
-        try (DataInputStream input =
-                     new DataInputStream(
-                             new BufferedInputStream(
-                                     new FileInputStream(fileName)
-                             )
-                     )
-        ) {
-            while (input.available() > 0) {
-                arrList.add(input.readInt());
-            }
-
-            //printList
-            for (Integer i : arrList) {
-                System.out.printf("%d ", i);
-                sum += i;
-            }
-            System.out.printf("%navg=%f", sum / arrList.size());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return sum;
     }
 
     private static void writeInFile(String fileName) {
@@ -69,9 +35,51 @@ public class TaskA {
                      )
         ) {
             for (int i = 0; i < 20; i++) {
-                output.write((int) (Math.random() * 1000 + i));
+                output.writeInt((int) (Math.random() * 10));
             }
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void readToArrayList(String fileName, List<Integer> arrList, double sum) {
+        try (DataInputStream input =
+                     new DataInputStream(
+                             new BufferedInputStream(
+                                     new FileInputStream(fileName)
+                             )
+                     )
+        ) {
+            while (input.available() > 0) {
+                arrList.add(input.readInt());
+            }
+
+            //Печатает список в консоль
+            printList(arrList, sum);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printList(List<Integer> arrList, double sum) {
+        for (Integer i : arrList) {
+            System.out.printf("%d ", i);
+            sum += i;
+        }
+        //DecimalFormat dc = new DecimalFormat("#.########E##");
+        //String output = dc.format(sum / arrList.size());
+        //System.out.println(output);
+        System.out.printf("%navg=%f", sum / arrList.size());
+    }
+
+    private static void writeInputToFile(String fileName, List<Integer> arrList, double sum) {
+        try (PrintWriter print = new PrintWriter(fileName)) {
+            for (Integer i : arrList) {
+                print.printf("%d ", i);
+                sum += i;
+            }
+            print.printf("%navg=%e", sum / arrList.size());
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
