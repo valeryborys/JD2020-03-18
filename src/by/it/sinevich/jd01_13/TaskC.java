c9089ce2c1b57edf682c73a17d579409fd027ec8
sinevich <sinevichaleksandr@gmail.com>
2020-04-17 09:58:52 +0300

    jd01-13 A-C

--- src/by/it/sinevich/jd01_13/TaskC.java
+++ src/by/it/sinevich/jd01_13/TaskC.java
@@ -1,8 +1,5 @@
 package by.it.sinevich.jd01_13;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
