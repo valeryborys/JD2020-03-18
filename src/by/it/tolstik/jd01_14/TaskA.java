d85b044af97a7b5aacfc74dc491e37b49e7a404b
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-18 15:40:36 +0300

    finished A jd01_14

--- src/by/it/tolstik/jd01_14/TaskA.java
+++ src/by/it/tolstik/jd01_14/TaskA.java
@@ -1,7 +1,54 @@
 package by.it.tolstik.jd01_14;
 
-public class TaskA {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class TaskA {
+
+    private static String dir(Class<?> aClass) {
+        String path = System.getProperty("user.dir") + File.separator
+                + "src" + File.separator;
+        String cDir = aClass.getName().replace(aClass.getSimpleName(), "")
+                .replace(".", File.separator);
+        return path + cDir;
+    }
+
+    private static void randomInput() {
+        try (DataInputStream dis = new DataInputStream
+                (new BufferedInputStream
+                        (new FileInputStream(dir(TaskA.class) + "dataTaskA.bin")));
+             PrintWriter pw = new PrintWriter
+                     (new FileWriter(dir(TaskA.class) + "resultTaskA.txt"))
+        ) {
+            double sum = 0;
+            double count = 0;
+            while (dis.available() > 0) {
+                int i = dis.readInt();
+                System.out.print(i + " ");
+                sum += i;
+                count++;
+            }
+            System.out.println("\navg=" + sum / count);
+            pw.println("\navg=" + sum / count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void randomOutput() {
+        try (DataOutputStream dos = new DataOutputStream
+                (new BufferedOutputStream
+                        (new FileOutputStream(dir(TaskA.class) + "dataTaskA.bin")))) {
+            for (int i = 0; i < 20; i++) {
+                dos.writeInt((int) (Math.random() * 10000));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        randomOutput();
+        randomInput();
     }
 }
