7754db062d7399187bce132c47644ab3f6eaea76
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-19 17:19:46 +0300

    finished C jd01_14

--- src/by/it/tolstik/jd01_14/TaskC.java
+++ src/by/it/tolstik/jd01_14/TaskC.java
@@ -5,6 +5,35 @@ import java.util.ArrayList;
 
 class TaskC {
 
+    private static final ArrayList<String> listWithFileNames = new ArrayList<>();
+    private static String resultDir = "";
+
+    public static void main(String[] args) {
+        getListFilesAndDir(getPath(TaskC.class, "jd01_14"));
+        recordInFile("resultTaskC.txt");
+        printFilesToConsole();
+        System.out.println(TaskC.resultDir);
+    }
+
+    //@param dir = name current directory for printing by step up in your directory;
+    private static String getPath(Class<?> aClass, String dir) {
+        return System.getProperty("user.dir") + File.separator + "src" + File.separator
+                + aClass.getPackageName().replace(dir, "")
+                .replace(".", File.separator);
+    }
+
+    private static void getListFilesAndDir(String path) {
+        File file = new File(path);
+        for (File elem : file.listFiles()) {
+            if (elem.isFile()) {
+                listWithFileNames.add(elem.getName());
+            } else if (elem.isDirectory()) {
+                resultDir += "dir:" + elem.getName() + "\n";
+                getListFilesAndDir(elem.getAbsolutePath());
+            }
+        }
+    }
+
     private static String pathForWriteFile(Class<?> aClass) {
         String path = System.getProperty("user.dir") + File.separator
                 + "src" + File.separator;
@@ -14,48 +43,22 @@ class TaskC {
         return path + cDir;
     }
 
-    private static ArrayList<String> listWithFileNames = new ArrayList<>();
-    private static String resultDir = "";
-
-    //@param dir = name current directory;
-    private static String getPath(Class<?> aClass, String dir) {
-        return System.getProperty("user.dir") + File.separator + "src" + File.separator
-                + aClass.getPackageName().replace(dir, "")
-                .replace(".", File.separator);
-    }
-
-    private static void getFiles(String path) {
-
-        File file = new File(path);
-        for (File elem : file.listFiles()) {
-            if (elem.isFile()) {
-                listWithFileNames.add(elem.getName());
-            } else if (elem.isDirectory()) {
-                resultDir += "dir:" + elem.getName() + "\n";
-                System.out.printf("dir:%s\n", elem.getName());
-                getFiles(elem.getAbsolutePath());
-            }
+    private static void printFilesToConsole() {
+        for (String names : listWithFileNames) {
+            System.out.printf("file:%s\n", names);
         }
-
     }
 
-    public static void main(String[] args) {
-        getFiles(getPath(TaskC.class, "jd01_14"));
-        try (PrintWriter pw = new PrintWriter(new FileWriter(pathForWriteFile(TaskC.class)+"resultTaskC.txt"))) {
+    private static void recordInFile(String name) {
+        try (PrintWriter pw = new PrintWriter
+                (new FileWriter(pathForWriteFile(TaskC.class) + name))) {
             pw.print(resultDir);
             for (String names : listWithFileNames) {
                 pw.printf("file:%s\n", names);
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.err.println("Ошибка " + e);
         }
-        for (String names : listWithFileNames) {
-            System.out.printf("file:%s\n", names);
-        }
-        System.out.println(TaskC.resultDir);
     }
-
-
 }
 
