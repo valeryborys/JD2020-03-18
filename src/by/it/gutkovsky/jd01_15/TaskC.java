2cd21653462aca0dd273b52a6d418f03b4f3e2a7
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-21 23:56:23 +0300

    Gutkovsky JD01_15 TaskС start

---
+++ src/by/it/gutkovsky/jd01_15/TaskC.java
@@ -0,0 +1,26 @@
+package by.it.gutkovsky.jd01_15;
+
+import java.util.Scanner;
+
+class TaskC {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("end")){
+                break;
+            }
+
+            if (input.equals("dir")){
+
+            }
+
+
+
+        }
+
+
+    }
+}
