5c29fe8ca836301bf1688fced01cda9e5f10f2eb
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-04-23 02:08:33 +0300

    - Задание С - в процессе

---
+++ src/by/it/okatov/jd02_01/JFChart.java
@@ -0,0 +1,49 @@
+package by.it.okatov.jd02_01;
+
+import by.it.okatov.jd01_15.TaskB;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class JFChart {
+    public static void main(String[] args) {
+        XYSeries series = new XYSeries("Number of buyers");
+        double a, b;
+
+        try (Scanner sc = new Scanner(
+                new File(
+                        TaskB.getFileName(
+                                Supermarket.class, "Chart.txt"
+                        )
+                )
+
+        )
+        ) {
+            while (sc.hasNext()) {
+                series.add(sc.nextInt(), sc.nextInt());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        JFreeChart chart = ChartFactory.createXYLineChart("time/number",
+                "time",
+                "number of buyers",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true, true, true);
+        ChartFrame frame = new ChartFrame("123", chart);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
