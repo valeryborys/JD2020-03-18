318a1cd5224ef7586fb4599a4a64eac84bc0dbe7
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-22 04:08:57 +0300

    finish B jd01_15

--- src/by/it/tolstik/jd01_15/TempCodeTaskB.java
+++ src/by/it/tolstik/jd01_15/TempCodeTaskB.java
@@ -1,8 +1,9 @@
 package by.it.tolstik.jd01_15;
 
 import java.io.*;
+import java.util.Scanner;
 
-class TempCodeTaskB {
+class TempCodeTaskB implements ConstForChar{
 
     private static String name = "TaskB.txt";
 
@@ -33,12 +34,51 @@ class TempCodeTaskB {
                 (new FileReader(dir(TempCodeTaskB.class) + "TempCodeTaskB.java"))) {
             String output;
             while ((output = br.readLine()) != null) {
+                char[] chars = output.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    if (chars[i] == a1) {
+                        if (chars[i+1] == a1) {
+                            for (int j = i; j < chars.length; j++) {
+                                chars[j] = ' ';
+                                output = String.valueOf(chars[j]);
+                            }
+                        }
+                        if (chars[i+1] == a3) {
+                            if (output.indexOf(a3,2) == -1) {
+                                for (int j = i; j < chars.length; j++) {
+                                    chars[j] = ' ';
+                                    output = String.valueOf(chars[j]);
+                                }
+                            }
+                            else {
+                                for (int j = i; j < chars.length; j++) {
+                                    if (chars[j] == a1) {
+                                        chars[j] = ' ';
+                                        output = String.valueOf(chars[j]);
+                                        break;
+                                    }
+                                    else {
+                                        chars[j] = ' ';
+                                    }
+                                    output = String.valueOf(chars[j]);
+                                }
+                            }
+                        }
+                    }
+                    output = String.valueOf(chars[i]);
+
+                }
+
+
+
+
                 if (output.contains("/") | output.contains("*")) {
                     sb.append("");
                 } else {
                     sb.append(output).append("\n");
                 }
             }
+
             System.out.println(sb);
         } catch (IOException e) {
             e.getStackTrace();
