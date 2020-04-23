f359000d171425be7ec6e35871de00a97f3fb8d1
Verbitsky Serhey <training.v.sergey@gmail.com>
2020-04-23 12:16:45 +0300

    jd02-01 Task C - fix some algorithms

--- src/by/it/verbitsky/jd02_01/Helper.java
+++ src/by/it/verbitsky/jd02_01/Helper.java
@@ -3,13 +3,15 @@ package by.it.verbitsky.jd02_01;
 import java.util.Random;
 
 class Helper {
-    //для ускорения процессов нужно увеличить K_SPEED (ТОЛЬКО ДЛЯ ПРОВЕРКИ ЗАДАНИЯ)
-    public static int kSpeed = 1;
+    //Чем больше коэффициент - тем больше ускоряются действия покупателей,
+    // при 1 - коэффициент не учитывается при определении задержек
+    public static int accelerationFactor = 1;
 
 
     //коэффициент для ускорения/замедления процессов
     //в зависимости от времени и кол-ва покупателей
-    public static int speedFactor = 0;
+    //более плавное замедление
+    public static int slowFactor = 0;
 
     private static Random generator = new Random();
 
@@ -26,7 +28,7 @@ class Helper {
 
     public static int getRandomTimeout(int start, int end, double buyerSpeedFactor) {
         return (int) (Math.round(
-                (start + generator.nextInt(end - start + 1) * buyerSpeedFactor) + speedFactor) / kSpeed);
+                (start + generator.nextInt(end - start + 1) * buyerSpeedFactor) + slowFactor) / accelerationFactor);
     }
 
     public static int getRandom(int start, int end) {
@@ -37,54 +39,56 @@ class Helper {
         return generator.nextDouble() * 100;
     }
 
+
+    //фактор регулирования потока покупателей в зависимости от времени и кол-ва покупателей в магазине
     public static int getCountFactor(int currentSec, int buyerCount) {
         int factor = 0;
 
         if (currentSec % 60 <= 30) {
             if (buyerCount < 8) {
                 //людей мало - можно немного увеличить таймауты = задержать людей
-                factor = 2;
-                speedFactor = 1000;
-                kSpeed = 1;
+                factor = 3;
+                slowFactor = 2000;
+                accelerationFactor = 1;
             }
-
+            //среднее кол-во людей
             if (buyerCount > 8 && buyerCount < 12) {
                 factor = 1;
-                speedFactor = 0;
+                slowFactor = 1000;
             }
 
             if (buyerCount > 12) {
                 //людей много - гужно ускорить тех кто уже в магазине
                 factor = 0;
-                speedFactor = 0;
-                kSpeed = 5;
+                slowFactor = 0;
+                accelerationFactor = 4;
             }
         } else {
             if (buyerCount < 30) {
                 //людей очень мало - можно немного увеличить таймауты = задержать людей
-                factor = 4;
-                speedFactor = 1000;
-                kSpeed = 1;
+                factor = 6;
+                slowFactor = 1500;
+                accelerationFactor = 1;
             }
             //людей по прежнему маловато
             if (buyerCount > 30 && buyerCount < 35) {
-                factor = 2;
-                speedFactor = 500;
-                kSpeed = 1;
+                factor = 4;
+                slowFactor = 1000;
+                accelerationFactor = 1;
             }
 
             if (buyerCount > 35 && buyerCount < 40) {
-                //людей достаточно - гужно немного ускорить тех кто уже в магазине
-                factor = 1;
-                speedFactor = 200;
-                kSpeed = 3;
+                //людей достаточно - нужно немного ускорить тех кто уже в магазине
+                factor = 3;
+                slowFactor = 500;
+                accelerationFactor = 2;
             }
 
             if (buyerCount > 40) {
                 //людей слишком много - гужно ускорить тех кто уже в магазине
                 factor = 0;
-                speedFactor = 0;
-                kSpeed = 5;
+                slowFactor = 0;
+                accelerationFactor = 5;
             }
         }
         return factor;
--- src/by/it/verbitsky/jd02_01/Shop.java
+++ src/by/it/verbitsky/jd02_01/Shop.java
@@ -1,10 +1,14 @@
 package by.it.verbitsky.jd02_01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Shop {
     private static Offer shopOffer;
 
     //можно попробовать сделать volatile
     private static volatile ShopManager shopManager;
+    private static List<Buyer> threads = new ArrayList<>();
 
 
     public static void main(String[] args) {
@@ -31,18 +35,28 @@ class Shop {
     private static void openShop() {
         int num = 0;
         System.out.println("Shop opened\n\n");
-        for (int i = 0; i < 120; i++) {
-            //каждую секунду запускаем от 0 до 2-х покупателей
-            int countEntered = Helper.getCountFactor(i, shopManager.getBuyers().size());
-            int pensionerFactor = Helper.getRandom(1, 4);
-            boolean oldMan = false;
+        for (int i = 0; i < 60; i++) {
+            //каждую секунду запускаем несколько покупателей (зависит от времени и кол-ва которые уже находятся в магазине
+            int countEntered = Helper.getCountFactor(i, getShopManager().getBuyers().size());
+            boolean oldMan = false;//флаг для определения пенсионера
             for (int j = 0; j < countEntered; j++) {
-                //создали поток (но он еще не запущен)
-                if (pensionerFactor == 4) {
+                if (num % 4 == 0) {
+                    //если покупатель 4-й, то с вероятностью 50/50 он будет пенсионер, но если нет, то 5-й будет точно
+                    int coin = Helper.getRandom(1, 2); //подкидываем монетку
+                    if (coin == 1) {
+                        oldMan = true; //покупатель будет пенсионер
+                    }
+                }
+                //если 4-й покупатель не был пенсионером, то 5-й будет обязательно
+                if (num % 5 == 0 && !oldMan) {
                     oldMan = true;
                 }
+                //создали поток (но он еще не запущен)
+
                 Buyer buyer = new Buyer(++num, oldMan);
-                shopManager.addBuyer(buyer);
+                oldMan = false; //обнуляем флаг
+                getShopManager().addBuyer(buyer);
+                threads.add(buyer);
                 //запускаем поток
                 buyer.start();
             }
@@ -51,25 +65,33 @@ class Shop {
                 это гарантия того, что мы принимаем покупателей 2 минуты (цикл 120 раз, 1000мс = 1 секунда - всего 2 минуты)
 
                 //в зависимости от временного интервала и кол-ва покупателей
-                  ускоряем либо замедляем процессы у покупателей, путем изменения
-                  speedfactor'a - пока сделал через кол-во покупателей за секунду, а не через увеличение задержек
-             */
-
+                  ускоряем либо замедляем процессы у покупателей, путем изменения speedfactor'a
+            */
 
             Helper.sleep(1000);
-            System.out.println("Прошло секунд " + i % 60);
+            System.out.println("---------------------" +
+                    "Прошло секунд " + i % 60);
 
             System.out.println("---------------------" +
-                    "Всего покупателей в магазине: " + shopManager.getBuyers().size());
+                    "Всего покупателей в магазине: " + getShopManager().getBuyers().size());
         }
 
         //теперь нужна задержка для того, чтобы закрепить потоки покупателей перед основным поток main
         //слипаем на пару мс основной поток и после этого закрепляем потоки
 
-        Helper.sleep(5000);
-
-
-        for (Buyer buyer : shopManager.getBuyers()) {
+        Helper.sleep(3000);
+        for (Buyer buyer : threads) {
+            try {
+                //закрепляем поток каждого покупателя перед основным поток выполнения
+                //чтобы главный поток магазина не закрылся ранньше чем потоки покупателей
+                buyer.join();
+            } catch (InterruptedException e) {
+                //throw new RuntimeException(e);
+                System.out.println(e.getMessage());
+            }
+        }
+/*
+        for (Buyer buyer : getShopManager().getBuyers()) {
             try {
                 //закрепляем поток каждого покупателя перед основным поток выполнения
                 //чтобы главный поток магазина не закрылся ранньше чем потоки покупателей
@@ -78,6 +100,8 @@ class Shop {
                 throw new RuntimeException(e);
             }
         }
+        */
+
     }
 
     //вернуть предложения товаров с ценами
