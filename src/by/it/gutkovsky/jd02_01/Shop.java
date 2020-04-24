97ac6c2fece82d788ddec195f782c29a16061d49
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-24 19:49:37 +0300

    Gutkovsky JD02_01 TaskC - part2 in progress - rev1

--- src/by/it/gutkovsky/jd02_01/Shop.java
+++ src/by/it/gutkovsky/jd02_01/Shop.java
@@ -14,15 +14,34 @@ class Shop {
 
         int number = 0;
         List<Buyer> buyers = new ArrayList<>();
-        for (int time = 0; time < 120; time++) {
-            int count = Helper.getRandom(0, 2);
-            for (int i = 0; i < count; i++) {
-                number++;
-                buyers.add(creatCustomer(number, buyers));
+        for (int time = 0; time < 31; time++) {
+            int count; // quantity of buyers that will enter to the shop
+
+            if (time >= 0 && time <=30) {
+                if(Manager.checkingQuantityInShop() < (time + 10)) {
+                    count = time+10-Manager.checkingQuantityInShop();
+                    for (int i = 0; i < count; i++) {
+                        number++;
+                        buyers.add(creatCustomer(number,buyers));
+                    }
+                }
+
+
             }
+
+
+
+            // it was before start C part 2
+//            count = Helper.getRandom(0, 2);
+//            for (int i = 0; i < count; i++) {
+//                number++;
+//                buyers.add(creatCustomer(number, buyers));
+//            }
             mapForChecking.put(time, Manager.checkingQuantityInShop()); // temprory method method that helping to test
             Helper.sleep(1000);
         }
+
+        Helper.sleep(2000);
 
         for (Buyer buyer : buyers) {
             try {
