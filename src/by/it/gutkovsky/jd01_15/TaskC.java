a3f10a0237dc101d2c427ed61f662de3ce47643b
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-25 19:33:13 +0300

    Gutkovsky JD02_01 TaskC - part2 - completed

--- src/by/it/gutkovsky/jd01_15/TaskC.java
+++ src/by/it/gutkovsky/jd01_15/TaskC.java
@@ -3,24 +3,27 @@ package by.it.gutkovsky.jd01_15;
 import java.util.Scanner;
 
 class TaskC {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         while (true) {
-            String input = sc.nextLine();
-            if (input.equals("end")){
+            String input = sc.nextLine().trim();
+            if (input.equals("end")) {
                 break;
             }
-
-            if (input.equals("dir")){
-
+            if (input.equals("cd")) {
+                System.out.println(Commands.path); // печатаем текущий путь
+            } else if (input.equals("cd ..")) {
+                System.out.println(Commands.getNewDirectory()); // подымаемся  на каталог вверх
+            } else if (input.equals("dir")) {
+                Commands.printDirProperties(Commands.path); // печатаем содержимое папки
+            } else if (input.contains(" ")) {
+                System.out.println(Commands.getNewDirectory(input));
+            } else {
+                System.out.println("\"" + input + "\"" + "  не является внутренней или внешней \n" +
+                        "командой, исполняемой программой или пакетным файлом.\n");
             }
-
-
-
         }
-
-
     }
 }
