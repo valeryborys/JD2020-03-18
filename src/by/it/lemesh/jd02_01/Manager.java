ea4538a900a4462923596e16b39d1fa9cfa83f03
Sergey Lemesh <serrgey.l@gmail.com>
2020-04-26 14:34:44 +0300

    jd02_01 comleted

---
+++ src/by/it/lemesh/jd02_01/Manager.java
@@ -0,0 +1,17 @@
+package by.it.lemesh.jd02_01;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Manager {
+    static final int K_SPEED = 10;
+    private static Map<Integer, Integer> treeMap = new TreeMap<>();
+
+    public static void report(int time) {
+        treeMap.put(time, Buyer.buyersOnline);
+    }
+
+    public static void print() {
+        System.out.println(treeMap.toString());
+    }
+}
