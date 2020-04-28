d610d98472a2f673da3495c384414824c1972052
Vitaly Tolstik <vital.tol90@gmail.com>
2020-04-28 15:09:31 +0300

    added full C jd02_01

--- src/by/it/tolstik/jd02_01/Shop.java
+++ src/by/it/tolstik/jd02_01/Shop.java
@@ -4,19 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Shop {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Магазин открылся");
         int number = 1;
         List<Buyer> buyers = new ArrayList<>();
-        for (int time = 0; time < 120; time++) {
-            int count = Helper.getRandom(0, 2);
-            for (int i = 0; i <= count; i++) {
+        for (int time = 0; time < 120;) {
+            if (time <= 30) {
                 Buyer buyer = new Buyer(number++);
                 buyer.start();
                 buyers.add(buyer);
+                Helper.sleep(3000,100);
+                time += 3;
+            } else if (time <=60) {
+                Buyer buyer = new Buyer(number++);
+                buyer.start();
+                buyers.add(buyer);
+                Helper.sleep(1000,100);
+                time++;
+            } else if (time <=90) {
+                Buyer buyer = new Buyer(number++);
+                buyer.start();
+                buyers.add(buyer);
+                Helper.sleep(3000,100);
+                time += 3;
+            } else {
+                Buyer buyer = new Buyer(number++);
+                buyer.start();
+                buyers.add(buyer);
+                Helper.sleep(1000,100);
+                time++;
             }
-            Helper.sleep(1000, 1000);
         }
+
         for (Buyer buyer : buyers) {
             try {
                 buyer.join();
