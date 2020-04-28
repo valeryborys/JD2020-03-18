1986f8ee0a11397cadf7b238775ec80ce95974a4
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-04-28 20:41:23 +0300

    - Окончательно очистил код от предупреждений

--- src/by/it/okatov/jd02_02/Cart.java
+++ src/by/it/okatov/jd02_02/Cart.java
@@ -4,32 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
-    private Map<String, Integer> goodsInCart = new HashMap<>();
+    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+    private final Map<String, Integer> goodsInCart = new HashMap<>();
 
-    public Map<String, Integer> getGoodsInCart() {
-        return goodsInCart;
-    }
-
-    public void setGoodsInCart(Map<String, Integer> goodsInCart) {
-        this.goodsInCart = goodsInCart;
-    }
-
-    Cart() {
-
-    }
-
-    Cart(Map<String, Integer> goodsInCart) {
-        this.goodsInCart = goodsInCart;
-    }
-
-    Cart(Map.Entry<String, Integer> entry) {
-        this.goodsInCart.put(entry.getKey(), entry.getValue());
-    }
-
-    Map<String, Integer> addGoodsToCart(Map.Entry<String, Integer> entry) {
-        this.goodsInCart.put(entry.getKey(), entry.getValue());
-        return goodsInCart;
-    }
 
     void clearCart() {
         this.goodsInCart.clear();
