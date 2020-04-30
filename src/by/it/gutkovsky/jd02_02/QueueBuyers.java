28d9b56bf52eaa8ad636d08ed47d5fcde069776b
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-04-30 18:14:09 +0300

    Gutkovsky JD02_02 completed

--- src/by/it/gutkovsky/jd02_02/QueueBuyers.java
+++ src/by/it/gutkovsky/jd02_02/QueueBuyers.java
@@ -2,32 +2,36 @@ package by.it.gutkovsky.jd02_02;
 
 import java.util.ArrayDeque;
 
+
 class QueueBuyers {
 
     static final Object MONITOR_BUYERS = new Object();
 
-    private static final ArrayDeque<Buyer> queue = new ArrayDeque<>();
-    private static final ArrayDeque<Buyer> pensioneerQueue = new ArrayDeque<>();
+        private static final ArrayDeque<Buyer> queue = new ArrayDeque<>();
+        private static final ArrayDeque<Buyer> pensioneerQueue = new ArrayDeque<>();
 
-    static synchronized void  add(Buyer buyer){
-        if (buyer.isPensioner()) {
-            pensioneerQueue.addLast(buyer);
-        } else {
-            queue.addLast(buyer);
+        static synchronized void  add(Buyer buyer){
+            if (buyer.isPensioner()) {
+                pensioneerQueue.addLast(buyer);
+            } else {
+                queue.addLast(buyer);
+            }
         }
-    }
 
-    static synchronized Buyer extract(){
-        if(pensioneerQueue.size() > 0) {
-            return pensioneerQueue.pollFirst();
+        static synchronized Buyer extract(){
+            if(pensioneerQueue.size() > 0) {
+                return pensioneerQueue.pollFirst();
+            }
+            else return queue.pollFirst();
         }
-        else return queue.pollFirst();
-    }
 
-    static synchronized int queueSize(){
-//        synchronized (MONITOR_BUYERS) {
-            return queue.size() + pensioneerQueue.size();
-//        }
-    }
+        static synchronized int queueSize(){
+    //        synchronized (MONITOR_BUYERS) {
+                return queue.size() + pensioneerQueue.size();
+    //        }
+        }
+
+
+
 
 }
