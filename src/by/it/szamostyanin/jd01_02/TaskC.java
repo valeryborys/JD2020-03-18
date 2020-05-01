f54d021a48ba871d56c76f1c48a874f60faa6304
szamostyanin <sergeyzamostyanin@gmail.com>
2020-05-01 21:40:29 +0300

    jd01_02 TaskC1,C2,C3 Completed

--- src/by/it/szamostyanin/jd01_02/TaskC.java
+++ src/by/it/szamostyanin/jd01_02/TaskC.java
@@ -1,14 +1,17 @@
 package by.it.szamostyanin.jd01_02;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] mas = step1(n);
         step1(n);
-        //step2();
-        //step3();
+        step2(mas);
+        step3(mas);
+
     }
 
     private static int[][] step1(int n) {
@@ -27,19 +30,85 @@ public class TaskC {
                     if (matrix[i][j] == -1 * n) {
                         state2 = true;
                     }
-                    System.out.print(matrix[i][j] + " ");
                 }
-                System.out.print("\n");
             }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.print("\n");
         }
         return matrix;
     }
 
     private static int step2(int[][] mas) {
-        return 0;
+        int sum = 0;
+        int first,second;
+        for (int i = 0; i < mas.length; i++) {
+            first = 0;
+            second = 0;
+            for (int j = 0; j < mas.length; j++) {
+                if (mas[i][j] > 0) {    //находим первый положительный элемент
+                    first = j;
+                    break;
+                }
+            }
+            for (int j = first + 1; j < mas.length; j++) {
+                if (mas[i][j] > 0) {    //находим второй положительный элемент
+                    second = j;
+                    break;
+                }
+            }
+            for (int j = first + 1; j < second; j++) {
+                sum += mas[i][j];   //находим их сумму
+            }
+        System.out.println("Sum = " + sum);
     }
+        return sum;
+}
 
-    private static int[][] step3(int[][] mas) {
-        return null;
+    static int[][] step3(int[][] array) {
+        int max = Integer.MIN_VALUE;
+        for (int[] row : array) {
+            for (int element : row) {
+                if (element > max) {
+                    max = element;
+                }
+            }
+        }
+        boolean[] delRow = new boolean[array.length];
+        boolean[] delCol = new boolean[array[0].length];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == max) {
+                    delRow[i] = true;
+                    delCol[j] = true;
+                }
+            }
+        }
+
+        int nRow = 0;
+        int nCol = 0;
+        for (boolean del : delRow) if (!del) nRow++;
+        for (boolean del : delCol) if (!del) nCol++;
+
+        int[][] result = new int[nRow][nCol];
+
+        int ir = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!delRow[i]) {
+                int ic = 0;
+                for (int j = 0; j < array[i].length; j++) {
+                    if (!delCol[j]) {
+                        result[ir][ic++] = array[i][j];
+                    }
+                }
+                ir++;
+            }
+        }
+        return result;
     }
 }
