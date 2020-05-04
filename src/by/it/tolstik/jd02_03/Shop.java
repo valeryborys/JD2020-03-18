485f9cc27d6f013ea9dbd18aa8396a3ec7a295f8
Vitaly Tolstik <vital.tol90@gmail.com>
2020-05-04 03:32:49 +0300

    start jd02_03

--- src/by/it/tolstik/jd02_03/Shop.java
+++ src/by/it/tolstik/jd02_03/Shop.java
@@ -2,6 +2,8 @@ package by.it.tolstik.jd02_03;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class Shop {
     public static void main(String[] args) {
@@ -11,33 +13,26 @@ class Shop {
         int time = 0;
         int number = 0;
 
-        List<Thread> threads = new ArrayList<>();
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+
         while (Manager.shopOpen()) {
             synchronized (QueueBuyers.MONITOR) {
                 if (Cashier.getCashiersOpened() < QueueBuyers.getCashNeed()) {
                     Cashier cashier = new Cashier(Cashier.getCashiersOpened() + 1);
-                    Thread thread = new Thread(cashier);
-                    threads.add(thread);
-                    thread.start();
+                    threadPool.execute(cashier);
+//TODO 2:09:49
                 }
             }
 
             Buyer buyer = new Buyer(++number);
             buyer.start();
-            threads.add(buyer);
             time++;
             if (time <= 10) Helper.sleep(3000, 100);
             else if (time <= 50) Helper.sleep(1000, 100);
             else if (time <= 60) Helper.sleep(3000, 100);
             else Helper.sleep(1000, 100);
         }
-        for (Thread buyer : threads) {
-            try {
-                buyer.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException("Interrupted" + Thread.currentThread(), e);
-            }
-        }
+
         Manager.getTotalSum();
         System.out.println("Магазин закрылся. Кол-во посетителей: " + number);
     }
