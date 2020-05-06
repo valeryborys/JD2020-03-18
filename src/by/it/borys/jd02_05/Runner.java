8d4d9e62c8ee88d20b81623566875f0db1ac9edf
Valery <valeryborys@gmail.com>
2020-05-06 22:41:19 +0300

    jd02_05 A

---
+++ src/by/it/borys/jd02_05/Runner.java
@@ -0,0 +1,18 @@
+package by.it.borys.jd02_05;
+
+
+import java.util.Locale;
+
+public class Runner {
+    public static void main(String[] args) {
+        ResMan res = ResMan.INSTANCE;
+        if (args.length ==2){
+            Locale locale = new Locale(args[0], args[1]);
+            res.setLocale(locale);
+        }
+        System.out.println(res.get(Message.hello));
+        System.out.println(res.get(Message.question));
+        System.out.println(res.get(User.firstName));
+        System.out.println(res.get(User.lastName));
+    }
+}
