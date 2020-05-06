723c87811bac3ca6448a0c392d51b67f57a004f8
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-05-06 14:38:05 +0300

    - jd02_05 - TaskB - добавил изменение цвета в консоли + локализовал строку ошибки при вводе неправильной локализации

--- src/by/it/okatov/jd02_05/Message.java
+++ src/by/it/okatov/jd02_05/Message.java
@@ -3,4 +3,7 @@ package by.it.okatov.jd02_05;
 public interface Message {
     String hello = "message.hello";
     String question = "message.question";
+    String inputQuestion = "message.inputQuestion";
+    String inputLocale = "message.inputLocale";
+    String msgErrorInputNotLocale = "message.errorNotLocale";
 }
--- src/by/it/okatov/jd02_05/Runner.java
+++ src/by/it/okatov/jd02_05/Runner.java
@@ -1,19 +1,60 @@
 package by.it.okatov.jd02_05;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Runner {
+    static boolean localeExists = false;
+
     public static void main(String[] args) {
         ResourcesManager manager = ResourcesManager.INSTANCE;
+        Locale locale = Locale.getDefault();
+
         if (args.length == 2) {
-            Locale locale = new Locale(args[0], args[1]);
+            locale = new Locale(args[0], args[1]);
             manager.setLocale(locale);
         }
 
+        Scanner sc = new Scanner(System.in);
+        String locInput;
+        while (true) {
+            System.out.println(manager.getString(Message.inputQuestion));
+            System.out.print(manager.getString(Message.inputLocale) + " ");
+            locInput = sc.nextLine();
+            switch (locInput) {
+                case Commands.be:
+                    locale = new Locale(locInput, "BY");
+                    localeExists = true;
+                    break;
+                case Commands.en:
+                    locale = new Locale(locInput, "US");
+                    localeExists = true;
+                    break;
+                case Commands.ru:
+                    locale = new Locale(locInput, "RU");
+                    localeExists = true;
+                    break;
+                case "end":
+                    return;
+                default:
+                    String s = manager.getString(Message.msgErrorInputNotLocale);
+                    System.out.println("\u001B[33m" + s);
+                    System.out.println("\u001B[0m");
+                    localeExists = false;
+                    break;
+            }
+            manager.setLocale(locale);
 
-        System.out.println(manager.getString(Message.hello));
-        System.out.println(manager.getString(Message.question));
-        System.out.println(manager.getString(User.name));
-        System.out.println(manager.getString(User.surname));
+            if (localeExists) {
+                System.out.println(manager.getString(Message.hello));
+                System.out.println(manager.getString(Message.question));
+                System.out.println(manager.getString(User.name));
+                System.out.println(manager.getString(User.surname));
+                localeExists = false;
+            }
+
+        }
+
+
     }
 }
