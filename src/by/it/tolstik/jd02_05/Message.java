bb1262c502e2f31cb1e6428b4d9a90582a1a5bec
Vitaly Tolstik <vital.tol90@gmail.com>
2020-05-07 16:26:04 +0300

    finish A jd02_05

---
+++ src/by/it/tolstik/jd02_05/Message.java
@@ -0,0 +1,6 @@
+package by.it.tolstik.jd02_05;
+
+interface Message {
+    String hello="message.hello";
+    String question="message.question";
+}
--- src/by/it/tolstik/jd02_05/Runner.java
+++ src/by/it/tolstik/jd02_05/Runner.java
@@ -1,7 +1,18 @@
 package by.it.tolstik.jd02_05;
 
+import java.util.Locale;
+
 class Runner {
     public static void main(String[] args) {
+        ResMan instance = ResMan.INSTANCE;
+        if(args.length == 2) {
+            Locale locale = new Locale(args[0], args[1]);
+            instance.setLocale(locale);
+        }
 
+        System.out.println(instance.get(Message.hello));
+        System.out.println(instance.get(Message.question));
+        System.out.println(instance.get(User.firstName));
+        System.out.println(instance.get(User.lastName));
     }
 }
