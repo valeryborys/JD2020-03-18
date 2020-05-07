2b2a0aa181ee6905ce7bebf0ca4e09d647a5abea
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-05-07 16:06:58 +0300

    Gutkovsky JD02_05 TaskB completed

--- src/by/it/gutkovsky/jd02_05/txt/universal.bat
+++ src/by/it/gutkovsky/jd02_05/txt/universal.bat
@@ -1,4 +1,4 @@
 set path="C:\Program Files\Java\jdk1.8.0_231\bin"
 FOR %%I In (*.txt) DO (
-native2ascii.exe -encoding utf-8 %%I ../res/%%~nI.properties
+native2ascii.exe -encoding utf-8 %%I ../resources/%%~nI.properties
 )
