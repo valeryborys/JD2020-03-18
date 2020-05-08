570bb6a0c3ee0b5c4e43d60c42176dff7813fec7
PsyXodelic <beavkostya@gmail.com>
2020-05-08 04:45:45 +0300

    jd02_04

--- src/by/it/bobrovich/calc/ParserTest.java
+++ src/by/it/bobrovich/calc/ParserTest.java
@@ -7,11 +7,65 @@ import static org.junit.Assert.*;
 public class ParserTest {
 
     @Test
-    public void calc() throws CalcException {
+    public void calcTestA1() throws Exception {
         double expected = 7.3;
         Parser parser = new Parser();
         Var varA = parser.calc("A=2+5.3");
         double value = Double.parseDouble(varA.toString());
         assertEquals(expected, value, 1e-8);
+    }
+
+    @Test
+    public void calcTestA2() throws Exception {
+        double expected = 25.55;
+        Parser parser = new Parser();
+        Var varA = parser.calc("B=A*3.5");
+        double value = Double.parseDouble(varA.toString());
+        assertEquals(expected, value, 1e-8);
+    }
+
+    @Test
+    public void calcTestA3() throws Exception {
+        double expected = 25;
+        Parser parser = new Parser();
+        Var varA = parser.calc("B1=B+0.11*-5");
+        double value = Double.parseDouble(varA.toString());
+        assertEquals(expected, value, 1e-8);
+    }
+
+    @Test
+    public void calcTestA4() throws Exception {
+        double expected = 2.65;
+        Parser parser = new Parser();
+        Var varA = parser.calc("B2=A/2-1");
+        double value = Double.parseDouble(varA.toString());
+        assertEquals(expected, value, 1e-8);
+    }
+
+    @Test
+    public void calcTestB1() throws Exception {
+        double expected = 40.15;
+        Parser parser = new Parser();
+        Var varA = parser.calc("C=B+(A*2)");
+        double value = Double.parseDouble(varA.toString());
+        assertEquals(expected, value, 1e-8);
+    }
+
+    @Test
+    public void calcTestB2() throws Exception {
+        double expected = 10;
+        Parser parser = new Parser();
+        Var varA = parser.calc("D=((C-0.15)-20)/(7-5)");
+        double value = Double.parseDouble(varA.toString());
+        assertEquals(expected, value, 1e-8);
+    }
+
+    @Test
+    public void calcTestB3() throws Exception {
+        double[] expected = {10, 15};
+        Parser parser = new Parser();
+        Var varB = parser.calc("E={2,3}*(D/2)");
+        double[] value = ((Vector) varB).getValue();
+        assertArrayEquals(expected, value, 1e-8);
     }
 }
--- src/by/it/bobrovich/calc/Var.java
+++ src/by/it/bobrovich/calc/Var.java
@@ -1,34 +1,50 @@
 package by.it.bobrovich.calc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Var implements Operation {
 
-    static Var createVar(String value) throws CalcException{
-        if(value.matches(Patterns.SCALAR))
+    private static Map<String, Var> list = new HashMap<>();
+
+    static Var createVar(String value) throws CalcException {
+        value = value.replace(" ", "");
+        if (value.matches(Patterns.SCALAR))
             return new Scalar(value);
-        if(value.matches(Patterns.VECTOR))
+        if (value.matches(Patterns.VECTOR))
             return new Vector(value);
-        if(value.matches(Patterns.MATRIX))
+        if (value.matches(Patterns.MATRIX))
             return new Matrix(value);
-        throw new CalcException("Impossible to create " + value);
+        else {
+            Var var = list.get(value);
+            if(var != null)
+                return var;
+            else
+                throw new CalcException("Impossible to create " + value);
+        }
     }
 
     @Override
-    public Var add(Var other) throws CalcException{
+    public Var add(Var other) throws CalcException {
         throw new CalcException("Impossible operation");
     }
 
     @Override
-    public Var sub(Var other) throws CalcException{
+    public Var sub(Var other) throws CalcException {
         throw new CalcException("Impossible operation");
     }
 
     @Override
-    public Var mul(Var other) throws CalcException{
+    public Var mul(Var other) throws CalcException {
         throw new CalcException("Impossible operation");
     }
 
     @Override
-    public Var div(Var other) throws CalcException{
+    public Var div(Var other) throws CalcException {
         throw new CalcException("Impossible operation");
+    }
+
+    public static void save(String name, Var var) {
+        list.put(name, var);
     }
 }
