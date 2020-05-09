69c959ab62b294d9c7353a931d329c9f9c16064b
PsyXodelic <beavkostya@gmail.com>
2020-05-09 19:13:07 +0300

    jd02_05

--- src/by/it/bobrovich/calc/ConsoleRunner.java
+++ src/by/it/bobrovich/calc/ConsoleRunner.java
@@ -9,22 +9,27 @@ public class ConsoleRunner {
         String line;
         Parser parser = new Parser();
         Printer printer = new Printer();
+        label:
         while(true){
             line = scanner.nextLine();
-            if(line.equals("end"))
-                break;
-            else if(line.equals("be")){
-                Locale locale = new Locale("be", "BY");
-                ResMan.setLocale(locale);
-                continue;
-            }else if(line.equals("ru")){
-                Locale locale = new Locale("ru", "RU");
-                ResMan.setLocale(locale);
-                continue;
-            }else if(line.equals("en")){
-                Locale locale = new Locale("en", "US");
-                ResMan.setLocale(locale);
-                continue;
+            switch (line) {
+                case "end":
+                    break label;
+                case "be": {
+                    Locale locale = new Locale("be", "BY");
+                    ResMan.setLocale(locale);
+                    continue label;
+                }
+                case "ru": {
+                    Locale locale = new Locale("ru", "RU");
+                    ResMan.setLocale(locale);
+                    continue label;
+                }
+                case "en": {
+                    Locale locale = new Locale("en", "US");
+                    ResMan.setLocale(locale);
+                    continue label;
+                }
             }
             try {
                 printer.print(parser.calc(line));
